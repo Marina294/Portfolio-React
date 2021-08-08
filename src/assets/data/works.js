0f10dd83a8b961c99abca8fcf9839c1a850c3d3d
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-08-08 00:24:26 -0700

    changed link

--- src/assets/data/works.js
+++ src/assets/data/works.js
@@ -1,16 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 import DrawingTimeImg from '../images/work1.png';
-import GreenCtgImg from '../images/greenctg.jpg';
-import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
-import ProjectImg from '../images/projectImg.png';
 
 const works = [
   {
     id: uuidv4(),
     name: 'Drawing Time!',
     desc: 'An application to Draw illustration. Desktop Only.',
-    link: 'https://heuristic-kepler-007f68.netlify.app/',
+    link: 'https://drawing-time.netlify.app/',
     img: DrawingTimeImg,
   },
   {
@@ -19,7 +15,7 @@ const works = [
     desc:
       'An app to help people to get an overview of how they can make the city beautiful.',
     link: 'https://www.linkedin.com/in/marina-yamamoto-558276128/',
-    img: GreenCtgImg,
+    img: DrawingTimeImg,
   },
   {
     id: uuidv4(),
@@ -27,7 +23,7 @@ const works = [
     desc:
       'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
     link: 'https://www.linkedin.com/in/marina-yamamoto-558276128/',
-    img: CoinTrackerImg,
+    img: DrawingTimeImg,
   },
   {
     id: uuidv4(),
@@ -35,7 +31,7 @@ const works = [
     desc:
       'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
     link: 'https://www.linkedin.com/in/marina-yamamoto-558276128/',
-    img: CavinImg,
+    img: DrawingTimeImg,
   },
   {
     id: uuidv4(),
@@ -43,7 +39,7 @@ const works = [
     desc:
       'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
     link: 'https://www.linkedin.com/in/marina-yamamoto-558276128/',
-    img: ProjectImg,
+    img: DrawingTimeImg,
   },
 ];
 
