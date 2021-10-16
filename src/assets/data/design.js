691a447f7916c0393330a2208e08c7c39950fb0a
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-10-15 23:17:03 -0700

    deleted uuidv4

--- src/assets/data/design.js
+++ src/assets/data/design.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import CalculatorImg from '../images/calculator.jpg';
 import RockPaperMeowImg from '../images/rock.jpg';
 import WhatTOCookTodayImg from '../images/cook.jpg';
@@ -6,25 +6,25 @@ import CheckWorldTimeImg from '../images/clock.jpg';
 
 const design = [
   {
-    id: uuidv4(),
+    // id: uuidv4(),
     name: 'Web Design',
     link: '/Design#webDesign',
     img: CheckWorldTimeImg,
   },
   {
-    id: uuidv4(),
+    // id: uuidv4(),
     name: 'Online Ads',
     link: '/Design#onlineAds',
     img: WhatTOCookTodayImg,
   },
   {
-    id: uuidv4(),
+    // id: uuidv4(),
     name: 'Catalog Design',
     link: '/Design#catalogDesign',
     img: RockPaperMeowImg,
   },
   {
-    id: uuidv4(),
+    // id: uuidv4(),
     name: 'Art Direction',
     link: '/Design#artDirection',
     img: CalculatorImg,
