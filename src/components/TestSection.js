266d54f6e97b7aaad8e59ede87f2120c8d56ccd6
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-10-21 18:08:02 -0700

    test8

--- src/components/TestSection.js
+++ src/components/TestSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import PText from './PText';
-import WorkImg1 from '../assets/images/w-paidy-website1.jpg';
-import WorkImg2 from '../assets/images/w-paidy-website2.jpg';
+import WorkImg1 from '../assets/images/w-event1.jpg';
+import WorkImg2 from '../assets/images/w-event2.jpg';
+import WorkImg3 from '../assets/images/w-event3.jpg';
+import WorkImg4 from '../assets/images/w-event4.jpg';
 
 const WebDesignStyles = styled.div`
   padding: 5rem 0;
@@ -70,11 +72,11 @@ const WebDesignStyles = styled.div`
 
 export default function WebDesign() {
   return (
-    <WebDesignStyles id="webDesign">
+    <WebDesignStyles id="promotional">
       <div className="container">
         <div>
           <div>
-            <h2 className="webSection__heading">Web Design</h2>
+            <h2 className="webSection__heading">Promotional Materials</h2>
             <h4 className="works__subheading">
               Renewal the Corprate website / Company Branding
             </h4>
@@ -100,8 +102,8 @@ export default function WebDesign() {
               Concept, Design
             </div>
             <div className="workItem__img">
-              <img src={WorkImg1} alt="work img" />
-              <img src={WorkImg2} alt="work img" />
+              <img src={WorkImg3} alt="work img" />
+              <img src={WorkImg4} alt="work img" />
             </div>
           </div>
         </div>
