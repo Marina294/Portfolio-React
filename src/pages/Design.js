86f30102fd1c7294ead06882a4794de06bf21c61
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-10-21 17:14:06 -0700

    promo2

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -7,7 +7,7 @@ import WebDesignSection from '../components/WebDesignSection';
 import OnlineAdsSection from '../components/OnlineAdsSection';
 import CatalogDesignSection from '../components/CatalogDesignSection';
 import MagazineDesignSection from '../components/MagazineDesignSection';
-// import PromotionalSection from '../components/PromotionalSection';
+import PromotionalSection from '../components/PromotionalSection';
 
 const DesignStyle = styled.div`
   padding: 10rem 0;
@@ -45,7 +45,7 @@ export default function Design() {
       <WebDesignSection />
       <CatalogDesignSection />
       <MagazineDesignSection />
-      {/* <PromotionalSection /> */}
+      <PromotionalSection />
     </DesignStyle>
   );
 }
