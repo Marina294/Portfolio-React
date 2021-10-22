e57c608d33c62b7fd0cbe41657f0dc91f2ffc026
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-10-21 17:51:22 -0700

    test3

--- src/components/promotionalSection.js
+++ src/components/promotionalSection.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import PText from './PText';
 import WorkImg1 from '../assets/images/w-event1.jpg';
 import WorkImg2 from '../assets/images/w-event2.jpg';
-import WorkImg3 from '../assets/images/w-event3.jpg';
-import WorkImg4 from '../assets/images/w-event4.jpg';
+// import WorkImg3 from '../assets/images/w-event3.jpg';
+// import WorkImg4 from '../assets/images/w-event4.jpg';
 
 const PromotionalSectionStyles = styled.div`
   padding: 5rem 0;
@@ -98,7 +98,7 @@ export default function PromotionalSection() {
             <img src={WorkImg1} alt="work img" />
             <img src={WorkImg2} alt="work img" />
           </div>
-          <div className="works__infoDetail">
+          {/* <div className="works__infoDetail">
             <br /> Client: Japan Nailist Association
             <br /> Date: 04/2017 <br />
             Work: Art Direction, Design
@@ -106,7 +106,7 @@ export default function PromotionalSection() {
           <div className="workItem__img">
             <img src={WorkImg3} alt="work img" />
             <img src={WorkImg4} alt="work img" />
-          </div>
+          </div> */}
         </div>
       </div>
     </PromotionalSectionStyles>
